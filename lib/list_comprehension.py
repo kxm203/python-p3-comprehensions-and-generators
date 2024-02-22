869ed53ba74ed4d0cd1d2aad37f26a7749209f86
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-02-22 16:48:04 -0500

    Pretty cool!

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    if even_numbers:
+        return even_numbers
+    else:
+        return [] 
+
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        return []
+    exclaimed_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclaimed_sentences
